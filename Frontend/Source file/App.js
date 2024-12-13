ee53c01068ecbfb1d6802af039017b5f9ada6846
lordeske8 <eskicmihajlo55@gmail.com>
2024-12-13 23:21:21 +0100

    kreirana login strana

--- Frontend/Source file/App.js
+++ Frontend/Source file/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+
+import Login from './komponente/Login'; 
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';  
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
